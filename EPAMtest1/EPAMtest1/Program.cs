5378a609ca7f98d5cfc2a58e2d6bf8e821be8fe9
boris111079@gmail.com <boris111079@gmail.com>
2020-04-18 23:43:33 +0300

    First

---
+++ EPAMtest1/EPAMtest1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EPAMtest1
+{// 1 - вводится начало диапазона и конец-нужно что-бы программа посчитала сколько будет чисел,
+ //     в которых есть цифра 7. Начало 10 конец 30 (2 числа с 7-кой )
+   
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
